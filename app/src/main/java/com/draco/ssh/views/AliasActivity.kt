8a2c5a4aa2cb39d3a132fd6d5a35f0514c793dec
usg-ishimura <ideo.alessandro@protonmail.com>
2023-04-06 00:43:17 +0200

    first element always present

--- app/src/main/java/com/draco/ssh/views/AliasActivity.kt
+++ app/src/main/java/com/draco/ssh/views/AliasActivity.kt
@@ -22,7 +22,7 @@ class AliasActivity : AppCompatActivity() {
     private lateinit var plus: ExtendedFloatingActionButton
     private lateinit var ll: LinearLayout
     private lateinit var imgv: ImageView
-    private var id: Int = 0
+    private var id: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,92 +30,91 @@ class AliasActivity : AppCompatActivity() {
         setContentView(R.layout.activity_alias)
         plus = findViewById(R.id.addalias)
         ll = findViewById(R.id.dynamiclinearlayout)
-
+        addElemsProgrammatically(id)
 
         plus.setOnClickListener {
             // counter
             id++
-
-            // setting voice alias programmatically
-
-            val tilVoicealias = TextInputLayout(
-                ContextThemeWrapper(
-                    this,
-                    R.style.Widget_MaterialComponents_TextInputLayout_OutlinedBox
-                )
-            )
-
-            tilVoicealias.boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
-            tilVoicealias.boxBackgroundColor =
-                ContextCompat.getColor(tilVoicealias.context, android.R.color.transparent)
-            tilVoicealias.hint = "Voice alias"
-
-            val clpTextInputLayout = ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            tilVoicealias.layoutParams = clpTextInputLayout
-
-            clpTextInputLayout.topMargin = 10
-            clpTextInputLayout.marginStart = 16
-            clpTextInputLayout.marginEnd = 16
-
-            val edtVoicealias = TextInputEditText(tilVoicealias.context)
-            tilVoicealias.addView(edtVoicealias)
-
-            tilVoicealias.setBoxCornerRadii(10f, 10f, 10f, 10f)
-
-            // setting shell command programmatically
-
-            val tilShellcommand = TextInputLayout(
-                ContextThemeWrapper(
-                    this,
-                    R.style.Widget_MaterialComponents_TextInputLayout_OutlinedBox
-                )
-            )
-
-            tilShellcommand.boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
-            tilShellcommand.boxBackgroundColor = ContextCompat.getColor(tilVoicealias.context, android.R.color.transparent)
-            tilShellcommand.hint = "Shell command"
-
-            val clpTextInputLayoutSC = ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            tilShellcommand.layoutParams = clpTextInputLayoutSC
-
-            clpTextInputLayoutSC.topMargin = 0
-            clpTextInputLayoutSC.marginStart = 16
-            clpTextInputLayoutSC.marginEnd = 16
-
-            val edtShellcommand = TextInputEditText(tilShellcommand.context)
-            tilShellcommand.addView(edtShellcommand)
-
-            tilShellcommand.setBoxCornerRadii(10f, 10f, 10f, 10f)
-
-            // setting arrow down programmatically
-
-            imgv = ImageView(this)
-            imgv.setImageResource(R.drawable.baseline_arrow_downward_24)
-            imgv.layoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-
-            val valueTV = TextView(this)
-            valueTV.text = id.toString() + "."
-            val clpTextInputLayoutTV = ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-            valueTV.layoutParams = clpTextInputLayoutTV
-            clpTextInputLayoutTV.topMargin = 10
-            clpTextInputLayoutTV.marginStart = 16
-
-            // adding elements
-
-            ll.addView(valueTV)
-            ll.addView(tilVoicealias)
-            ll.addView(imgv)
-            ll.addView(tilShellcommand)
+            addElemsProgrammatically(id)
         }
+    }
+    fun addElemsProgrammatically(id: Int){
+        val tilVoicealias = TextInputLayout(
+            ContextThemeWrapper(
+                this,
+                R.style.Widget_MaterialComponents_TextInputLayout_OutlinedBox
+            )
+        )
 
+        tilVoicealias.boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
+        tilVoicealias.boxBackgroundColor =
+            ContextCompat.getColor(tilVoicealias.context, android.R.color.transparent)
+        tilVoicealias.hint = "Voice alias"
+
+        val clpTextInputLayout = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        tilVoicealias.layoutParams = clpTextInputLayout
+
+        clpTextInputLayout.topMargin = 10
+        clpTextInputLayout.marginStart = 16
+        clpTextInputLayout.marginEnd = 16
+
+        val edtVoicealias = TextInputEditText(tilVoicealias.context)
+        tilVoicealias.addView(edtVoicealias)
+
+        tilVoicealias.setBoxCornerRadii(10f, 10f, 10f, 10f)
+
+        // setting shell command programmatically
+
+        val tilShellcommand = TextInputLayout(
+            ContextThemeWrapper(
+                this,
+                R.style.Widget_MaterialComponents_TextInputLayout_OutlinedBox
+            )
+        )
+
+        tilShellcommand.boxBackgroundMode = TextInputLayout.BOX_BACKGROUND_OUTLINE
+        tilShellcommand.boxBackgroundColor = ContextCompat.getColor(tilVoicealias.context, android.R.color.transparent)
+        tilShellcommand.hint = "Shell command"
+
+        val clpTextInputLayoutSC = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        tilShellcommand.layoutParams = clpTextInputLayoutSC
+
+        clpTextInputLayoutSC.topMargin = 0
+        clpTextInputLayoutSC.marginStart = 16
+        clpTextInputLayoutSC.marginEnd = 16
+
+        val edtShellcommand = TextInputEditText(tilShellcommand.context)
+        tilShellcommand.addView(edtShellcommand)
+
+        tilShellcommand.setBoxCornerRadii(10f, 10f, 10f, 10f)
+
+        // setting arrow down programmatically
+
+        imgv = ImageView(this)
+        imgv.setImageResource(R.drawable.baseline_arrow_downward_24)
+        imgv.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        val valueTV = TextView(this)
+        valueTV.text = id.toString() + "."
+        val clpTextInputLayoutTV = ConstraintLayout.LayoutParams(
+            ConstraintLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        valueTV.layoutParams = clpTextInputLayoutTV
+        clpTextInputLayoutTV.topMargin = 10
+        clpTextInputLayoutTV.marginStart = 16
+
+        // adding elements
+
+        ll.addView(valueTV)
+        ll.addView(tilVoicealias)
+        ll.addView(imgv)
+        ll.addView(tilShellcommand)
     }
 }
